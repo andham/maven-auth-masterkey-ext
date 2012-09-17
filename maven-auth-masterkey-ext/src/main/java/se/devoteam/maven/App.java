c17ac3fb251caf99f8348570b2552de5952d9963
KarinKarlsson <karin.karlsson@devoteam.com>
2012-09-17 16:32:24 +0200

    Format changes

--- maven-auth-masterkey-ext/src/main/java/se/devoteam/maven/App.java
+++ maven-auth-masterkey-ext/src/main/java/se/devoteam/maven/App.java
@@ -4,10 +4,8 @@ package se.devoteam.maven;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
