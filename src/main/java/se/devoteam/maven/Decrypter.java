45ab10bd502680f343cb4f56bc8c74a8bfe432dd
KarinKarlsson <karin.karlsson@devoteam.com>
2012-09-19 16:36:48 +0200

    implemeted a solution using the security location property

--- src/main/java/se/devoteam/maven/Decrypter.java
+++ src/main/java/se/devoteam/maven/Decrypter.java
@@ -10,14 +10,24 @@
 package se.devoteam.maven;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.maven.settings.Proxy;
 import org.apache.maven.settings.Server;
+import org.apache.maven.settings.building.DefaultSettingsProblem;
 import org.apache.maven.settings.building.SettingsProblem;
+import org.apache.maven.settings.building.SettingsProblem.Severity;
 import org.apache.maven.settings.crypto.SettingsDecrypter;
 import org.apache.maven.settings.crypto.SettingsDecryptionRequest;
 import org.apache.maven.settings.crypto.SettingsDecryptionResult;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
+import org.sonatype.plexus.components.cipher.PlexusCipherException;
+import org.sonatype.plexus.components.sec.dispatcher.DefaultSecDispatcher;
+import org.sonatype.plexus.components.sec.dispatcher.SecDispatcher;
+import org.sonatype.plexus.components.sec.dispatcher.SecDispatcherException;
 
 /**
  * 
@@ -25,27 +35,97 @@ import org.apache.maven.settings.crypto.SettingsDecryptionResult;
  * @see #decrypt(SettingsDecryptionRequest)
  *
  */
+@Component( role = SettingsDecrypter.class ) //TODO: add this to components.xml
 public class Decrypter implements SettingsDecrypter {
+	
+	//TODO: let plexus handle this (figure out a way to create a test case with a plexus container)
+	@Requirement( hint = "maven" )
+    private SecDispatcher securityDispatcher;
+	
+	protected static final String SYSTEM_PROPERTY_SEC_LOCATION = "settings.security";
 	
 	/**
 	 * Decrypt supplied passwords.
 	 * @param request the passwords to decrypt
-	 * @return all of the decrypted passwords
+	 * @return all of the decrypted passwords as well as a list of encountered problems (if any)
 	 */
 	public SettingsDecryptionResult decrypt(SettingsDecryptionRequest request) {
 		
-		List<Server> servers = new ArrayList<Server>(request.getServers().size());
-		List<Proxy> proxies = new ArrayList<Proxy>(request.getProxies().size());
+		List<Server> clonedServers = cloneServers(request.getServers());
+		List<Proxy> clonedProxies = cloneProxies(request.getProxies());
 		List<SettingsProblem> problems = new ArrayList<SettingsProblem>();
 		
-		for (Server server : request.getServers()) {
-			servers.add(server.clone());
+		for (Server clonedServer : clonedServers) {
+			
+            try {
+            	clonedServer.setPassword(decrypt( clonedServer.getPassword()));
+            }
+            catch (SecDispatcherException e) {
+                problems.add( new DefaultSettingsProblem( "Failed to decrypt password for server " + clonedServer.getId()
+                    + ": " + e.getMessage(), Severity.ERROR, "server: " + clonedServer.getId(), -1, -1, e ) );
+            }
+			
 		}
 		
-		for (Proxy proxy : request.getProxies()) {
-			proxies.add(proxy.clone());
+		for (Proxy clonedProxy : clonedProxies) {
+            try
+            {
+            	clonedProxy.setPassword( decrypt( clonedProxy.getPassword() ) );
+            }
+            catch ( SecDispatcherException e )
+            {
+                problems.add( new DefaultSettingsProblem( "Failed to decrypt password for proxy " + clonedProxy.getId()
+                    + ": " + e.getMessage(), Severity.ERROR, "proxy: " + clonedProxy.getId(), -1, -1, e ) );
+            }
 		}
-		return getResult(servers,proxies,problems);
+		return getResult(clonedServers,clonedProxies,problems);
+	}
+	
+	
+	private List<Server> cloneServers(final List<Server> servers) {
+		List<Server> clonedServers = new ArrayList<Server>(servers.size());
+		for (Server server : servers) {
+			clonedServers.add(server.clone());
+		}
+		return clonedServers;
+	}
+	
+	private List<Proxy> cloneProxies(final List<Proxy> proxies) {
+		List<Proxy> clonedProxies = new ArrayList<Proxy>(proxies.size());
+		for (Proxy proxy : proxies ) {
+			clonedProxies.add(proxy.clone());
+		}
+		return clonedProxies;
+	}
+	
+	/**
+	 * Decrypts a string. The string must start with a <code>{</code> character and
+	 * end with a <code>}</code> in order to be decrypted. 
+	 * @param str the string to decrypt. 
+	 * @return str decrypted or just str if it dosen't cohere to expected format.
+	 * @throws SecDispatcherException decryption error
+	 */
+	private String decrypt(final String str) throws SecDispatcherException {
+		
+		if (str != null) {
+	
+			if (System.getProperty(SYSTEM_PROPERTY_SEC_LOCATION) != null) {
+				//securityDispatcher.decrypt(str);(
+				if (securityDispatcher == null) {
+					//TODO: let plexus handle this (figure out a way to create a test case with a plexus container)
+					securityDispatcher = new TmpSecDispatcher();
+					
+				}
+				return securityDispatcher.decrypt(str);
+			} else {
+				if (str.charAt(0) == '{' && str.charAt(str.length() - 1) == '}') {
+					throw new SecDispatcherException("Unable to decrypt password. The " + SYSTEM_PROPERTY_SEC_LOCATION + " must be set.");
+				}
+				//not an encrypted password
+			}
+		}
+		
+		return str;
 	}
 	
 	
@@ -80,6 +160,22 @@ public class Decrypter implements SettingsDecrypter {
 			}
 			
 		};
+	}
+	
+	/**
+	 * This is a temporary solution to make the tests work.
+	 * Remove this class when the plexus container is in place.
+	 */
+	public class TmpSecDispatcher extends DefaultSecDispatcher {
+		public TmpSecDispatcher() {
+	    	try {
+				super._cipher = new DefaultPlexusCipher();
+				super._decryptors = new HashMap();
+			} catch (PlexusCipherException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
